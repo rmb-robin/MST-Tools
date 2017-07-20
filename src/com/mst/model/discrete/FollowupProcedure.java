f8fff1235903efd121cde3bc03b97104919ddf6b
levk13 <l2kdevsolutions@gmail.com>
2017-07-19 22:29:22 -0500

    added new files.

---
+++ src/com/mst/model/discrete/FollowupProcedure.java
@@ -0,0 +1,20 @@
+package com.mst.model.discrete;
+
+public class FollowupProcedure {
+
+	private String edgeName; 
+	private String value;
+	
+	public String getEdgeName() {
+		return edgeName;
+	}
+	public void setEdgeName(String edgeName) {
+		this.edgeName = edgeName;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
