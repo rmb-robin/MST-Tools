9a962929a7b1b22aaf2938bb601aa251b7881bc2
levk13 <l2kdevsolutions@gmail.com>
2017-07-28 23:46:57 -0500

    update to reprocessing.

---
+++ src/com/mst/model/SentenceQuery/SentenceReprocessingInput.java
@@ -0,0 +1,21 @@
+package com.mst.model.SentenceQuery;
+
+public class SentenceReprocessingInput {
+
+	private String token; 
+	private String organizationId;
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getOrganizationId() {
+		return organizationId;
+	}
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	} 
+	
+	
+}
