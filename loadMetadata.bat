91d14835c0f1bab934309f41e316326a4a63446b
levk13 <l2kdevsolutions@gmail.com>
2017-07-31 22:58:25 -0500

    added bat

---
+++ loadMetadata.bat
@@ -0,0 +1,7 @@
+echo 'Running metadata upload'
+set basePath="C:\Users\user1\Documents\MST-tools\MST-Tools"
+
+'running upload...'
+cd %basePath%
+java -jar mongoLoader-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+pause
