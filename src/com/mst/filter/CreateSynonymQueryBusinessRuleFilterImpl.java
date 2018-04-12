e35aa804bff718331de569f8c56a490de48febc0
Brian Sheely <bsheely@medicalsearchtechnologies.com>
2018-04-12 15:28:57 -0400

    removed logging

--- src/com/mst/filter/CreateSynonymQueryBusinessRuleFilterImpl.java
+++ src/com/mst/filter/CreateSynonymQueryBusinessRuleFilterImpl.java
@@ -7,19 +7,12 @@ import com.mst.model.businessRule.QueryBusinessRule;
 import com.mst.model.discrete.DiscreteData;
 import com.mst.model.metadataTypes.CreateSynonymQueryBusinessRuleType;
 import com.mst.model.sentenceProcessing.SentenceDb;
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRuleFilter {
-    private static final Logger logger = LogManager.getLogger(CreateSynonymQueryBusinessRuleFilterImpl.class);
-
     @Override
     public List<SentenceQueryResult> filterByBusinessRule(List<SentenceQueryResult> nonFilteredResults, QueryBusinessRule rule) {
         return null;
@@ -27,20 +20,12 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
 
     @Override
     public CreateSynonymQueryBusinessRuleType filterByBusinessRule(SentenceQueryInput input, QueryBusinessRule businessRule) {
-        logger.setLevel(Level.WARN);
-        logger.warn("\n\n\n" + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()));
-        logger.warn("Entering filterByBusinessRule with " + input.getSentenceQueryInstances().size() + " SentenceQueryInstances, and " + businessRule.getRules().size() + " business rules.");
-
         List<QueryBusinessRule.Rule> rulesApplied = new ArrayList<>();
         try {
-
             List<QueryBusinessRule.Rule> rules = businessRule.getRules();
-            List<SentenceQueryInstance> queryInstances = input.getSentenceQueryInstances().stream().collect(Collectors.toList());
+            List<SentenceQueryInstance> queryInstances = new ArrayList<>(input.getSentenceQueryInstances());
 
             for (QueryBusinessRule.Rule rule : rules) {
-                logger.warn("Processing business rule - " + rule.getRuleName());
-                List<DiscreteDataType> discreteDataToMatch = rule.getDiscreteDataToMatch();
-
                 List<String> tokens = rule.getQueryTokens();
                 Map<String, List<String>> edgesToMatch = rule.getEdgeValuesToMatch();
                 String edgeName = rule.getEdgeName();
@@ -50,13 +35,7 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
                 boolean edgeExists = rule.isEdgeNameExists();
                 int index = 0;
 
-/*
-                ListIterator<SentenceQueryInstance> iter = queryInstances.listIterator();
-                while (iter.hasNext()) {
-                    SentenceQueryInstance queryInstance = iter.next();
-*/
-
-                    for (SentenceQueryInstance queryInstance : queryInstances) {
+                for (SentenceQueryInstance queryInstance : queryInstances) {
                     List<EdgeQuery> edgeQueries = queryInstance.getEdges();
                     boolean edgeNotFound = false;
                     if (queryInstance.getTokens() == null || queryInstance.getTokens().isEmpty())
@@ -78,7 +57,6 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
                                 newQueryInstance.setAppender("or");
                                 input.getSentenceQueryInstances().add(index + 1, newQueryInstance);
                                 rulesApplied.add(rule);
-                                logger.warn("New query instance added for " + synonymousEdgeQuery.getName());
                                 break;
                             } else if (!edgeExists)
                                 edgeNotFound = isEdgeToMatchNotFound(edgeQueries, edgeName);
@@ -88,29 +66,18 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
                     index++;
                     if (edgeNotFound) {
                         rulesApplied.add(rule);
-                        logger.warn("Rule applied: " + rule.getRuleName());
                     }
                 }
             }
         } catch (Exception e) {
-            StringWriter writer = new StringWriter();
-            PrintWriter printWriter = new PrintWriter(writer);
-            e.printStackTrace(printWriter);
-            printWriter.flush();
-            logger.warn("EXCEPTION - " + writer.toString());
+            printException(e);
         }
-
-
-        logger.warn("filterByBusinessRule returning: " + input.getSentenceQueryInstances().size() + " SentenceQueryInstances. " + rulesApplied.size() + " rules were applied.");
         return new CreateSynonymQueryBusinessRuleType(input, rulesApplied);
     }
 
     @Override
     public List<SentenceQueryResult> modifyByBusinessRule(List<SentenceQueryResult> sentenceQueryResults, List<QueryBusinessRule.Rule> rulesApplied) {
-        logger.warn("Entering modifyByBusinessRule with " + sentenceQueryResults.size() + " SentenceQueryResults, and " + rulesApplied.size() + " applied business rules.");
-        int count = 0;
         try {
-
             for (SentenceQueryResult sentenceQueryResult : sentenceQueryResults) {
                 DiscreteData resultDiscreteData = sentenceQueryResult.getDiscreteData();
                 List<SentenceQueryEdgeResult> edgeResults = sentenceQueryResult.getSentenceQueryEdgeResults();
@@ -123,7 +90,6 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
                     int maxRangeValue = -999;
 
                     if (ruleHasDiscreteData && resultDiscreteData != null) {
-                        logger.warn("Matching discrete data");
                         for (DiscreteDataType discreteData : discreteDataToMatch) {
                             minRangeValue = Integer.parseInt(discreteData.getMinRangeValue());
                             maxRangeValue = Integer.parseInt(discreteData.getMaxRangeValue());
@@ -140,20 +106,13 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
                         newEdgeResult.setEdgeName(rule.getSynonymousEdge());
                         newEdgeResult.setMatchedValue(rule.getSynonymousValue());
                         edgeResults.add(newEdgeResult);
-                        logger.warn("Added " + newEdgeResult.getEdgeName() + " edge with value " + newEdgeResult.getMatchedValue());
-                        count++;
                     }
                 }
             }
-        } catch (Exception e) {
-            StringWriter writer = new StringWriter();
-            PrintWriter printWriter = new PrintWriter(writer);
-            e.printStackTrace(printWriter);
-            printWriter.flush();
-            logger.warn("EXCEPTION - " + writer.toString());
         }
-
-        logger.warn("modifyByBusinessRule returning: " + sentenceQueryResults.size() + " SentenceQueryResults with " + count + " added edges");
+        catch (Exception e) {
+            printException(e);
+        }
         return sentenceQueryResults;
     }
 
@@ -196,7 +155,6 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
     private boolean areEdgesToMatchesFound(List<EdgeQuery> edgeQuery, Map<String, List<String>> edgesToMatch) {
         if (edgeQuery == null || edgeQuery.isEmpty())
             return false;
-
         for (Map.Entry<String, List<String>> entry : edgesToMatch.entrySet()) {
             if (!isEdgeToMatchFound(edgeQuery, entry.getKey(), entry.getValue()))
                 return false;
@@ -205,48 +163,22 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
     }
 
     private boolean isEdgeToMatchNotFoundInResults(List<SentenceQueryEdgeResult> edgeResults, String edgeToMatch) {
-        String log = "Edge to not match " + edgeToMatch;
-
         if (edgeResults != null && !edgeResults.isEmpty())
             for (SentenceQueryEdgeResult result : edgeResults) {
-                if (result != null && result.getEdgeName() != null && result.getEdgeName().equals(edgeToMatch)) {
-                    logger.warn(log + " was found in results");
+                if (result != null && result.getEdgeName() != null && result.getEdgeName().equals(edgeToMatch))
                     return false;
-                }
             }
-
-        logger.warn(log + " was not found in results");
         return true;
     }
 
     private boolean isEdgeToMatchFoundInResults(List<SentenceQueryEdgeResult> edgeResults, String edgeToMatch, List<String> valuesToMatch) {
-        String log = "Edge to match " + edgeToMatch;
         if (edgeResults != null && !edgeResults.isEmpty())
             for (SentenceQueryEdgeResult result : edgeResults) {
-                if (result != null && result.getEdgeName() != null && result.getEdgeName().equals(edgeToMatch)) {
-                    if (valuesToMatch != null && !valuesToMatch.isEmpty())
-                        log += " with value " + valuesToMatch.get(0);
-
-                    if (valuesToMatch == null || valuesToMatch.isEmpty() || valuesToMatch.contains(result.getMatchedValue())) {
-                        logger.warn(log += " was found in results");
+                if (result != null && result.getEdgeName() != null && result.getEdgeName().equals(edgeToMatch))
+                    if (valuesToMatch == null || valuesToMatch.isEmpty() || valuesToMatch.contains(result.getMatchedValue()))
                         return true;
-                    }
-                }
             }
-
-        logger.warn(log += " was not found in results");
         return false;
-    }
-
-    private boolean areEdgesToMatchesFoundInResults(List<SentenceQueryEdgeResult> edgeResults, Map<String, List<String>> edgesToMatch) {
-        if (edgeResults == null || edgeResults.isEmpty())
-            return false;
-
-        for (Map.Entry<String, List<String>> entry : edgesToMatch.entrySet()) {
-            if (!isEdgeToMatchFoundInResults(edgeResults, entry.getKey(), entry.getValue()))
-                return false;
-        }
-        return true;
     }
 
     //TODO if the ages in the sentences do not all fall within the age range specified in the rules, this will fail
@@ -259,7 +191,6 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
         for (DiscreteDataType discreteData : discreteDataToMatch) {
             //String name = discreteData.getName();
             DiscreteDataType.DataType dataType = discreteData.getDataType();
-
             if (dataType.equals(DiscreteDataType.DataType.NUMERIC_RANGE)) {
                 minRangeValue = Integer.parseInt(discreteData.getMinRangeValue());
                 maxRangeValue = Integer.parseInt(discreteData.getMaxRangeValue());
@@ -274,5 +205,13 @@ public class CreateSynonymQueryBusinessRuleFilterImpl implements QueryBusinessRu
                 return true;
         }
         return false;
+    }
+
+    private void printException(Exception e) {
+        StringWriter writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        e.printStackTrace(printWriter);
+        printWriter.flush();
+        System.out.print(writer.toString());
     }
 }
