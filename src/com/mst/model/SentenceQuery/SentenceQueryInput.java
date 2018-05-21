4f528b1618cfa1936dbd0c7b3a009e82721ef7c0
Brian Sheely <bsheely@medicalsearchtechnologies.com>
2018-05-20 21:54:02 -0400

    Added code for automated test framework

--- src/com/mst/model/SentenceQuery/SentenceQueryInput.java
+++ src/com/mst/model/SentenceQuery/SentenceQueryInput.java
@@ -1,86 +1,84 @@
 package com.mst.model.SentenceQuery;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.mst.jsonSerializers.ObjectIdJsonSerializer;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity("sentenceQueryInput")
 public class SentenceQueryInput {
+    @Id
+    @JsonSerialize(using = ObjectIdJsonSerializer.class)
+    private ObjectId id;
+    private boolean isNotAndAll;
+    private List<SentenceQueryInstance> sentenceQueryInstances;
+    private String organizationId;
+    private DiscreteDataFilter discreteDataFilter;
+    private boolean debug = false;
+    private boolean filterByReport = false;
+    private boolean filterByTokenSequence;
 
-	private boolean isNotAndAll;
-	
-	private List<SentenceQueryInstance> sentenceQueryInstances; 
-	private String organizationId; 
-	private DiscreteDataFilter discreteDataFilter;
-	private String debug = "false";
-	private boolean filterByReport = false;
-	private boolean filterByTokenSequence; 
-	
-	
-	public SentenceQueryInput(){
-		sentenceQueryInstances = new ArrayList<>();
-	}
-	
-	public List<SentenceQueryInstance> getSentenceQueryInstances() {
-		return sentenceQueryInstances;
-	}
-	public void setSentenceQueryInstances(List<SentenceQueryInstance> sentenceQueryInstances) {
-		this.sentenceQueryInstances = sentenceQueryInstances;
-	}
+    public SentenceQueryInput() {
+        sentenceQueryInstances = new ArrayList<>();
+    }
 
-	public String getOrganizationId() {
-		return organizationId;
-	}
+    public List<SentenceQueryInstance> getSentenceQueryInstances() {
+        return sentenceQueryInstances;
+    }
 
-	public String getDebug() {
-		return debug;
-	}
+    public void setSentenceQueryInstances(List<SentenceQueryInstance> sentenceQueryInstances) {
+        this.sentenceQueryInstances = sentenceQueryInstances;
+    }
 
-	public void setDebug(String debug) {
-		this.debug = debug;
-	}
+    public String getOrganizationId() {
+        return organizationId;
+    }
 
-	public boolean isFilterByReport() {
-		return filterByReport;
-	}
+    public boolean getDebug() {
+        return debug;
+    }
 
-	public void setFilterByReport(boolean filterByReport) {
-		this.filterByReport = filterByReport;
-	}
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
 
-	public void setOrganizationId(String organizationId) {
-		this.organizationId = organizationId;
-	}
+    public boolean isFilterByReport() {
+        return filterByReport;
+    }
 
-	public DiscreteDataFilter getDiscreteDataFilter() {
-		return discreteDataFilter;
-	}
+    public void setFilterByReport(boolean filterByReport) {
+        this.filterByReport = filterByReport;
+    }
 
-	public void setDiscreteDataFilters(DiscreteDataFilter discreteDataFilter) {
-		this.discreteDataFilter = discreteDataFilter;
-	}
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
 
-	public boolean getIsNotAndAll() {
-		return isNotAndAll;
-	}
+    public DiscreteDataFilter getDiscreteDataFilter() {
+        return discreteDataFilter;
+    }
 
-	public void setNotAndAll(boolean isNotAndAll) {
-		this.isNotAndAll = isNotAndAll;
-	}
+    public void setDiscreteDataFilters(DiscreteDataFilter discreteDataFilter) {
+        this.discreteDataFilter = discreteDataFilter;
+    }
 
-	public boolean isDebug() {
-		if ( debug == null ) 
-			return false;
-		try {
-			return Boolean.parseBoolean(debug);
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    public boolean getIsNotAndAll() {
+        return isNotAndAll;
+    }
 
-	public boolean isFilterByTokenSequence() {
-		return filterByTokenSequence;
-	}
+    public void setNotAndAll(boolean isNotAndAll) {
+        this.isNotAndAll = isNotAndAll;
+    }
 
-	public void setFilterByTokenSequence(boolean filterByTokenSequence) {
-		this.filterByTokenSequence = filterByTokenSequence;
-	}
+    public boolean isFilterByTokenSequence() {
+        return filterByTokenSequence;
+    }
+
+    public void setFilterByTokenSequence(boolean filterByTokenSequence) {
+        this.filterByTokenSequence = filterByTokenSequence;
+    }
 }
