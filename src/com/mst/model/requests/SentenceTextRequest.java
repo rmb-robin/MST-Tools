e719981c406c42bad5547624c363bd5c1d847b7d
Brian Sheely <bsheely@medicalsearchtechnologies.com>
2018-05-20 21:51:39 -0400

    Added code for automated test framework

--- src/com/mst/model/requests/SentenceTextRequest.java
+++ src/com/mst/model/requests/SentenceTextRequest.java
@@ -1,14 +1,27 @@
 package com.mst.model.requests;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.mst.jsonSerializers.ObjectIdJsonSerializer;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+@Entity("sentenceTextRequest")
 public class SentenceTextRequest extends SentenceRequestBase {
-	
-	private String text;
-	
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	
+    @Id
+    @JsonSerialize(using = ObjectIdJsonSerializer.class)
+    private ObjectId id;
+    private String text;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
