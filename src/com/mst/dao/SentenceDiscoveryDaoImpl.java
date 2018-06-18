894dd0e4205db39bf4dcfcff9cdb7b2b22c418ab
Brian Sheely <bsheely@medicalsearchtechnologies.com>
2018-06-18 14:49:46 -0400

    fixed compile error

--- src/com/mst/dao/SentenceDiscoveryDaoImpl.java
+++ src/com/mst/dao/SentenceDiscoveryDaoImpl.java
@@ -1,7 +1,6 @@
 package com.mst.dao;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.mongodb.morphia.Datastore;
@@ -10,54 +9,48 @@ import com.mst.interfaces.DiscreteDataDao;
 import com.mst.interfaces.MongoDatastoreProvider;
 import com.mst.interfaces.dao.SentenceDiscoveryDao;
 import com.mst.model.discrete.DiscreteData;
-import com.mst.model.recommandation.RecommendedTokenRelationship;
 import com.mst.model.recommandation.SentenceDiscovery;
-import com.mst.model.sentenceProcessing.SentenceDb;
 import com.mst.model.sentenceProcessing.SentenceProcessingFailures;
-import com.mst.sentenceprocessing.RecommendationEdgesVerificationProcesser;
+import com.mst.sentenceprocessing.RecommendationEdgesVerificationProcessor;
 
 public class SentenceDiscoveryDaoImpl extends BaseDocumentDaoImpl<SentenceDiscovery> implements SentenceDiscoveryDao {
+    private DiscreteDataDao discreteDataDao;
 
+    public SentenceDiscoveryDaoImpl() {
+        super(SentenceDiscovery.class);
+        discreteDataDao = new DiscreteDataDaoImpl();
 
-	private DiscreteDataDao discreteDataDao; 
-	
-	public SentenceDiscoveryDaoImpl() {
-		super(SentenceDiscovery.class);
-		discreteDataDao = new DiscreteDataDaoImpl();
-		
-	}
-	
-	@Override
-	public void setMongoDatastoreProvider(MongoDatastoreProvider provider) {
-		super.setMongoDatastoreProvider(provider);
-		discreteDataDao.setMongoDatastoreProvider(provider);
-	}
+    }
 
-	@Override
-	public void saveSentenceDiscovieries(List<SentenceDiscovery> sentenceDiscoveries) {
-		super.saveCollection(sentenceDiscoveries);	
-	}
-	
-	public void saveSentenceDiscoveries(List<SentenceDiscovery> sentenceDiscoveries, DiscreteData discreteData,SentenceProcessingFailures failures) {
-		Datastore ds = datastoreProvider.getDefaultDb();
-		if(discreteData!=null){
-				discreteDataDao.save(discreteData, false);
-				
-			for(SentenceDiscovery sentence: sentenceDiscoveries){
-				sentence.setDiscreteData(discreteData);
-				sentence.setOrganizationId(discreteData.getOrganizationId());
-				RecommendationEdgesVerificationProcesser.updateIndexesOnExisting(sentence.getWordEmbeddings(), sentence);
-				ds.save(sentence);
-			}
-		}
-	
-		if(failures!=null){
-			failures.setDate(LocalDate.now());
-			if(discreteData!=null){
-				failures.setDiscreteDataId(discreteData.getId().toString());
-				failures.setOrgId(discreteData.getOrganizationId());
-			}
-			ds.save(failures);
-		}
-	}
+    @Override
+    public void setMongoDatastoreProvider(MongoDatastoreProvider provider) {
+        super.setMongoDatastoreProvider(provider);
+        discreteDataDao.setMongoDatastoreProvider(provider);
+    }
+
+    @Override
+    public void saveSentenceDiscovieries(List<SentenceDiscovery> sentenceDiscoveries) {
+        super.saveCollection(sentenceDiscoveries);
+    }
+
+    public void saveSentenceDiscoveries(List<SentenceDiscovery> sentenceDiscoveries, DiscreteData discreteData, SentenceProcessingFailures failures) {
+        Datastore ds = datastoreProvider.getDefaultDb();
+        if (discreteData != null) {
+            discreteDataDao.save(discreteData, false);
+            for (SentenceDiscovery sentence : sentenceDiscoveries) {
+                sentence.setDiscreteData(discreteData);
+                sentence.setOrganizationId(discreteData.getOrganizationId());
+                RecommendationEdgesVerificationProcessor.updateIndexesOnExisting(sentence.getWordEmbeddings(), sentence);
+                ds.save(sentence);
+            }
+        }
+        if (failures != null) {
+            failures.setDate(LocalDate.now());
+            if (discreteData != null) {
+                failures.setDiscreteDataId(discreteData.getId().toString());
+                failures.setOrgId(discreteData.getOrganizationId());
+            }
+            ds.save(failures);
+        }
+    }
 }
